83842bf0d92faf362b9172978a875bdb66d9ba1f
NaveenGits <141821044+NaveenGits@users.noreply.github.com>
2023-08-09 14:45:38 +0530

    basic login
    basic login

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve user input
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// SQL injection prevention (optional, consider using prepared statements)
+$username = mysqli_real_escape_string($conn, $username);
+$password = mysqli_real_escape_string($conn, $password);
+
+// Query to check user credentials
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = $conn->query($query);
+
+if ($result->num_rows == 1) {
+    // Successful login
+    echo "Login successful!";
+} else {
+    // Failed login
+    echo "Login failed. Please check your username and password.";
+}
+
+// Close the database connection
+$conn->close();
+?>
