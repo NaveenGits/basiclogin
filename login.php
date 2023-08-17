3efe064e0a4efeaa505d41f44c4bf2ad2c48e18f
NaveenGits <141821044+NaveenGits@users.noreply.github.com>
2023-08-17 15:35:41 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,18 @@
 <?php
 // Database connection settings
-$username = "basiclogin";
-$password = "fresh86#";
-$dbname = "basicfunctiondb";
-
-// Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
-
-$connectionInfo = array("UID" => "basiclogindb", "pwd" => "{fresh86#}", "Database" => "basicfunctiondb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-
 $serverName = "tcp:basicloginfunction.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$connectionOptions = array(
+    "Database" => "basicfunctiondb",
+    "Uid" => "basiclogin",
+    "PWD" => "fresh86#"
+);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Establish a connection to SQL Server
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Check the connection
+if ($conn === false) {
+    die("Connection failed: " . print_r(sqlsrv_errors(), true));
 }
 
 // Retrieve user input
@@ -22,14 +20,18 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 // SQL injection prevention (optional, consider using prepared statements)
-$username = mysqli_real_escape_string($conn, $username);
-$password = mysqli_real_escape_string($conn, $password);
+$username = sqlsrv_real_escape_string($conn, $username);
+$password = sqlsrv_real_escape_string($conn, $password);
 
 // Query to check user credentials
 $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = $conn->query($query);
+$result = sqlsrv_query($conn, $query);
 
-if ($result->num_rows == 1) {
+if ($result === false) {
+    die("Query execution failed: " . print_r(sqlsrv_errors(), true));
+}
+
+if (sqlsrv_has_rows($result)) {
     // Successful login
     echo "Login successful!";
 } else {
@@ -38,5 +40,5 @@ if ($result->num_rows == 1) {
 }
 
 // Close the database connection
-$conn->close();
+sqlsrv_close($conn);
 ?>
