311eade639d7bc5f70024277d6c6144e3bb28ff9
NaveenGits <141821044+NaveenGits@users.noreply.github.com>
2023-08-18 12:27:40 +0530

    Update login.php

--- login.php
+++ login.php
@@ -26,28 +26,9 @@ if ($conn === false) {
     die("Connection failed: " . print_r(sqlsrv_errors(), true));
 }
 
-// Retrieve user input
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-// SQL injection prevention (consider using prepared statements)
-$username = sqlsrv_real_escape_string($conn, $username);
-$password = sqlsrv_real_escape_string($conn, $password);
-
-// Query to check user credentials
-$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-$result = sqlsrv_query($conn, $query);
-
-if ($result === false) {
-    die("Query execution failed: " . print_r(sqlsrv_errors(), true));
-}
-
-if (sqlsrv_has_rows($result)) {
-    // Successful login
-    echo "Login successful!";
-} else {
+else {
     // Failed login
-    echo "Login failed. Please check your username and password.";
+    echo "Connection success.";
 }
 
 // Close the database connections
